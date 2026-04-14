56ca2e7af896aa852e96e89f1ceab1579111f04d
Tadeo <tadeo.cabrer@alu.frlp.utn.edu.ar>
2026-04-14 08:49:31 -0300

    ejercicio-1-variables-operaciones

---
+++ ejercicios/c03-javascript/app.js
@@ -0,0 +1,13 @@
+
+const nombre = "Tadeo";
+const edad = 20;
+const materiaCursada = "Desarrollo de Software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materiaCursada}`);
+
+let contador = 0;
+
+contador++;
+contador++;
+contador++;
+console.log(contador); 
